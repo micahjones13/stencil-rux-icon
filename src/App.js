d76340e5643301a16f81a01044253889bad540b9
Micah Jones <micah.jones@rocketcom.com>
2021-08-25 12:54:44 -0700

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import "../node_modules/@astrouxds/astro-web-components/dist/astro-web-components/astro-web-components.css";
+
+import { RuxIcon, RuxButton } from "@astrouxds/astro-web-components";
+customElements.define("rux-icon", RuxIcon);
+customElements.define("rux-button", RuxButton);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <rux-icon icon="close" label="close" size="large"></rux-icon>
+      <rux-button size="large" icon="altitude">
+        Slotted Button
+      </rux-button>
     </div>
   );
 }
